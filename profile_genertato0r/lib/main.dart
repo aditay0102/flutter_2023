eb5f108955fb6457a0f454a3553385f9f0fc7f0c
aditya0102 <aditya0102.be21@chitkara.edu.in>
2023-01-22 14:39:31 +0530

    seperated the code using seprate class

--- profile_genertato0r/lib/main.dart
+++ profile_genertato0r/lib/main.dart
@@ -1,21 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new Material(
-    color: Colors.blue,
-    child: Center(
-      child: new Text("hello  Material!",
-          textDirection: TextDirection.ltr,
-          style: new TextStyle(
-              fontWeight: FontWeight.w800, fontStyle: FontStyle.italic)),
-    ),
-  )
-/*
-    new Center(
-        child: new Text(
-      'hello there',
-      textDirection: TextDirection.ltr,
-    )),
-    */
-      );
+  runApp(new MaterialApp(
+    title: "welcome back",
+    home: new Welcome(),
+  ));
+}
+
+class Welcome extends StatelessWidget {
+  const Welcome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return new Material(
+      color: Colors.blue,
+      child: Center(
+        child: new Text("hello  Material!",
+            textDirection: TextDirection.ltr,
+            style: new TextStyle(
+                fontWeight: FontWeight.w800, fontStyle: FontStyle.italic)),
+      ),
+    );
+    ;
+  }
 }
