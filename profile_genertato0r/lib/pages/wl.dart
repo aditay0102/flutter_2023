8bc37bcc2dc0435567798002f3cac9efe20c1f76
aditya0102 <aditya0102.be21@chitkara.edu.in>
2023-01-23 22:44:17 +0530

    made rows

--- profile_genertato0r/lib/pages/wl.dart
+++ profile_genertato0r/lib/pages/wl.dart
@@ -5,26 +5,24 @@ class Welcm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      color: Colors.green,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Container(
-                color: Colors.red,
-                child: Text("helo"),
-              ),
-              Container(
-                color: Colors.red,
-                child: Text("helo"),
-              ),
-            ],
+    return new Container(
+        color: Colors.greenAccent,
+        alignment: Alignment.center,
+        child:
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+          new Text(
+            "Item 1",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(fontSize: 18),
+          ),
+          new Text(
+            "Item 2",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(fontSize: 18),
+          ),
+          const Expanded(
+            child: const Text("Item3"),
           )
-        ],
-      ),
-    );
+        ]));
   }
 }
